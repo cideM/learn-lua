4615abe55e13fae988ab5ac29eccd0bd678edaa1
Florian Beeres <yuuki@protonmail.com>
2021-11-18 09:16:22 +0100

    update flake

--- flake.nix
+++ flake.nix
@@ -2,11 +2,13 @@
   description = "Programming in Lua";
 
   inputs.flake-utils.url = "github:numtide/flake-utils";
-  inputs.nixpkgs.url = "github:nixos/nixpkgs/staging-next";
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
 
   outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
-      let pkgs = nixpkgs.legacyPackages.${system}; in
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
       rec {
         devShell = pkgs.mkShell
           {
